dd67f3332b3812a101d15f8a5ce9962f6f5950e2
MichPerry-GG <michaelp@garagegames.com>
2013-04-19 11:55:40 -0400

    - LeapSDK integration: LeapToy WIP

--- modules/LeapToy/1/scripts/input.cs
+++ modules/LeapToy/1/scripts/input.cs
@@ -42,13 +42,13 @@ function LeapToy::initializeInput( %this )
     LeapMap.push();
     
     // Initialize the Leap Motion manager
-    //initLeapMotionManager();
-    //enableLeapMotionManager(true);
-    //enableLeapCursorControl(true);
+    initLeapMotionManager();
+    enableLeapMotionManager(true);
+    enableLeapCursorControl(true);
     
-    //configureLeapGesture("Gesture.Circle.MinProgress", 1.5);
-    //configureLeapGesture("Gesture.ScreenTap.MinForwardVelocity", 15);
-    //configureLeapGesture("Gesture.ScreenTap.MinDistance", 1.5);
+    configureLeapGesture("Gesture.Circle.MinProgress", 1.5);
+    configureLeapGesture("Gesture.ScreenTap.MinForwardVelocity", 15);
+    configureLeapGesture("Gesture.ScreenTap.MinDistance", 1.5);
 
     // Make this toy a listener for input (used purely for onTouchMoved in this case)
     SandboxWindow.addInputListener( %this );
